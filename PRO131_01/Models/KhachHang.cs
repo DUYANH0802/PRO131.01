eb22f8404307e6b302dd306c957154a84e0891d4
admin <admin@NGUYENDUYANH>
2025-08-07 20:43:35 +0700

    Add project files.

---
+++ PRO131_01/Models/KhachHang.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PRO131_01.Models;
+
+[Table("KhachHang")]
+public partial class KhachHang
+{
+    [Key]
+    public int MaKhachHang { get; set; }
+
+    [StringLength(100)]
+    public string? TenKhachHang { get; set; }
+
+    [StringLength(15)]
+    [Unicode(false)]
+    public string? SoDienThoai { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? Email { get; set; }
+
+    [StringLength(255)]
+    public string? DiaChi { get; set; }
+
+    [StringLength(10)]
+    public string? GioiTinh { get; set; }
+
+    [InverseProperty("MaKhachHangNavigation")]
+    public virtual ICollection<HoaDon> HoaDons { get; set; } = new List<HoaDon>();
+
+    [InverseProperty("MaKhachHangNavigation")]
+    public virtual ICollection<TaiKhoan> TaiKhoans { get; set; } = new List<TaiKhoan>();
+}
---
+++ PRO131_01/Models/SanPham.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PRO131_01.Models;
+
+[Table("SanPham")]
+public partial class SanPham
+{
+    [Key]
+    public int MaSanPham { get; set; }
+
+    [StringLength(100)]
+    public string? TenSanPham { get; set; }
+
+    public string? MoTa { get; set; }
+
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal? GiaBan { get; set; }
+
+    public int? SoLuongTonKho { get; set; }
+
+    [StringLength(255)]
+    public string? HinhAnh { get; set; }
+
+    public int? MaLoaiSanPham { get; set; }
+
+    public int? MaNhaCungCap { get; set; }
+
+    [InverseProperty("MaSanPhamNavigation")]
+    public virtual ICollection<HoaDonChiTiet> HoaDonChiTiets { get; set; } = new List<HoaDonChiTiet>();
+
+    [ForeignKey("MaLoaiSanPham")]
+    [InverseProperty("SanPhams")]
+    public virtual SanPhamChiTiet? MaLoaiSanPhamNavigation { get; set; }
+
+    [ForeignKey("MaNhaCungCap")]
+    [InverseProperty("SanPhams")]
+    public virtual NhaCungCap? MaNhaCungCapNavigation { get; set; }
+}
